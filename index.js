84a1a31b9c1723809e8b875124d771dca986822e
LehlohonoloKhoathane <140171463+LehlohonoloKhoathane@users.noreply.github.com>
2023-10-03 16:42:25 +0200

    added cookies

--- index.js
+++ index.js
@@ -105,3 +105,17 @@
         alert(`File name: ${file.name}`); 
         
       }
+
+      //cookies section
+      
+  setCookie = (cName, cValue, expDays) => {
+    let date = new Date();
+    date.setTime(date.getTime() + (expDays * 24 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = cName + "=" + cValue + ";" + expires + "; path=/";
+  }
+
+  document.querySelector("#cookiesBtn").addEventListener("click", () => {
+    document.querySelector("#cookies").style.display = "none";
+    setCookie("cookie", true, 30);
+  })
